8b75c5fc78fd2b2ff44fbeb2c246bc5693656c44
webia1 <webia1.github@gmail.com>
2023-06-18 21:48:13 +0200

    Angular Enterprise: Shared Module

--- apps/my-app1/src/app/app.module.ts
+++ apps/my-app1/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { appRoutes } from './app.routes';
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     BrowserAnimationsModule,
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent],
 })
