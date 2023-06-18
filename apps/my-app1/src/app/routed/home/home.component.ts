d4994d9719c0f30390fe5afb36af8866848ab542
webia1 <webia1.github@gmail.com>
2023-06-18 21:15:52 +0200

    Angular Enterprise: Home Component & Route

---
+++ apps/my-app1/src/app/routed/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'integrated-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
